f2527e048242ba48fe0bdab7185be7615162be9e
darhpreets <mrdsinghh@gmail.com>
2017-03-19 10:24:48 +0530

    Initial CHecking

--- check.cpp
+++ check.cpp
@@ -5,38 +5,27 @@
 
 using namespace std;
 
-int main () {
+int main () 
+{
   char ch;
   int count=0,  j=0;
   char head[200];
   fstream myfile ("final.csv" , ios::in | ios::out);
   fstream op0("new0.csv" , ios::out | ios::trunc | ios::in);
-fstream op1("new1.csv" , ios::out | ios::trunc | ios::in);
-  fstream op2("new2.csv" , ios::out | ios::trunc | ios::in);
-  fstream op3("new3.csv" , ios::out | ios::trunc | ios::in);
-  fstream op4("new4.csv" , ios::out | ios::trunc | ios::in);
-  fstream op5("new5.csv" , ios::out | ios::trunc | ios::in);
   
   if (myfile.is_open())
   {
-    int i=0;
      myfile.getline(head, 198, '\n');
   op0<<head;
-
-    // for(int i=0;head[i] !='\n';i++)
-    //   op<<head[i];
-
-
-    while ( myfile.get(ch) )
+    
+     while ( myfile.get(ch) )
     {
       switch (ch)
       {
       case '\n':
          count++;
-         if (count >500)
-         {
-
-         }
+  //       if (count >25)
+  
          op0<<ch;
 
        
@@ -61,17 +50,59 @@ fstream op1("new1.csv" , ios::out | ios::trunc | ios::in);
 
       }
 
-   } 
-
-
    myfile.close(); 
    op0.close();
 }
-
+}
 
   else 
     cout << "Unable to open file "; 
+
 cout<<endl<<"No. of lines are "<<count<<endl;
+cout<<"\tdas" << head << '\n';
 
   return 0;
 }
+
+
+  
+  /* 
+  fstream op1("new1.csv" , ios::out | ios::trunc | ios::in);
+
+if (myfile.is_open())
+  {
+    while ( myfile.get(ch) )
+    {
+      switch (ch)
+      {
+      case '\n':
+         count++;
+         if (count >25)
+         {
+            
+            op1<<head;
+         }
+         op0<<ch;
+
+       
+       break;
+
+       case ';':
+       op0<<',';
+       break;
+
+       case '"':
+       myfile.seekp(1,ios::cur);
+       if (ch =='"')
+        break;
+      else
+      {
+        myfile.seekp(-1,ios::cur);
+
+      }
+
+    default:
+    op0<<ch;
+
+      }
+*/
